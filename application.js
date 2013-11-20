32d859782cfc2bb6b3bd793a7d4d288d0fcf2d5d
Kevin Keiper <kevin@kevinkeiper.com>
2013-11-19 17:33:18 -0800

    Making variables out of my jquery selectors to not repeat myself

--- application.js
+++ application.js
@@ -1,48 +1,51 @@
 // when the page is ready
 $(document).ready(function(){
 	
+	var headerContainerH1 = $("#header-container h1");
+	var headerContainerH5 = $("header-container h5");
+	
 	// bind event to basecamp container
     $('#basecamp').mouseenter(function(){
-        $('#header-container h1').text("Basecamp is the project management tool you wish you had on your last project.");
-		$('#header-container h1').css("font-size", "44px");
-		$('#header-container h5').text("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.");
+        headerContainerH1.text("Basecamp is the project management tool you wish you had on your last project.");
+		headerContainerH1.css("font-size", "44px");
+		headerContainerH5.text("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.");
     });
         
     $('#basecamp').mouseleave(function(){
-        $('#header-container h1').text("Making collaboration productive and enjoyable for people every day.");
-		$('#header-container h1').css("font-size", "54px");
-		$('#header-container h5').text("Frustration free web based apps for colloboration, sharing information, and making decisions.");
-		$('#header-container h5').css("font-size", "20px");
+        headerContainerH1.text("Making collaboration productive and enjoyable for people every day.");
+		headerContainerH1.css("font-size", "54px");
+		headerContainerH5.text("Frustration free web based apps for colloboration, sharing information, and making decisions.");
+		headerContainerH5.css("font-size", "20px");
     });
 	
 	// bind event to highrise container
 	$('#highrise').mouseenter(function(){
-        $('#header-container h1').text("Highrise remembers the important things about people youd normally forget.");
-		$('#header-container h1').css("font-size", "44px");
-		$('#header-container h5').text("Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.");
+        headerContainerH1.text("Highrise remembers the important things about people youd normally forget.");
+		headerContainerH1.css("font-size", "44px");
+		headerContainerH5.text("Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.");
 		
     });
         
     $('#highrise').mouseleave(function(){
-        $('#header-container h1').text("Making collaboration productive and enjoyable for people every day.");
-		$('#header-container h1').css("font-size", "54px");
-		$('#header-container h5').text("Frustration free web based apps for colloboration, sharing information, and making decisions.");
-		$('#header-container h5').css("font-size", "20px");
+        headerContainerH1.text("Making collaboration productive and enjoyable for people every day.");
+		headerContainerH1.css("font-size", "54px");
+		headerContainerH5.text("Frustration free web based apps for colloboration, sharing information, and making decisions.");
+		headerContainerH5.css("font-size", "20px");
     });
 	
 	// bind event to campfire container
 	$('#campfire').mouseenter(function(){
-        $('#header-container h1').text("From near or far, helps teams work together over the web in real-time.");
-		$('#header-container h1').css("font-size", "44px");
-		$('#header-container h5').text("Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It’s game changing. We couldn’t run our own business without Campfire.");
+        headerContainerH1.text("From near or far, helps teams work together over the web in real-time.");
+		headerContainerH1.css("font-size", "44px");
+		headerContainerH5.text("Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It’s game changing. We couldn’t run our own business without Campfire.");
 		$('#header-container h5').css("font-size", "20px");
     });
         
     $('#campfire').mouseleave(function(){
-        $('#header-container h1').text("Making collaboration productive and enjoyable for people every day.");
-		$('#header-container h1').css("font-size", "54px");
-		$('#header-container h5').text("Frustration free web based apps for colloboration, sharing information, and making decisions.");
-		$('#header-container h5').css("font-size", "20px");
+        headerContainerH1.text("Making collaboration productive and enjoyable for people every day.");
+		headerContainerH1.css("font-size", "54px");
+		headerContainerH5.text("Frustration free web based apps for colloboration, sharing information, and making decisions.");
+		headerContainerH5.css("font-size", "20px");
     });
 	
 	
